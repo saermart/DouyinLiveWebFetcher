7a1d89b293050ac55c3fd47aa15a1094efea65e3
Y <kukushka@126.com>
2025-04-16 19:41:48 +0800

    测试成功

--- main.py
+++ main.py
@@ -9,7 +9,7 @@
 from liveMan import DouyinLiveWebFetcher
 
 if __name__ == '__main__':
-    live_id = '436920295491'
+    live_id = '844971802932'
     room = DouyinLiveWebFetcher(live_id)
     room.get_room_status()
     room.start()
