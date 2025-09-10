77a6980af0b678fd5d6c9c4bf09e773d5548bbd6
linzy <kukushka@126.com>
2025-09-10 16:40:26 +0800

    测试成功
    get_room_status失效

--- main.py
+++ main.py
@@ -9,11 +9,7 @@
 from liveMan import DouyinLiveWebFetcher
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    live_id = '294530521082'
-=======
-    live_id = '91046108832'
->>>>>>> 44655ce5a26e3eb11508cb7ba645b2967dee888b
+    live_id = '504448096643'
     room = DouyinLiveWebFetcher(live_id)
-    room.get_room_status()
+    # room.get_room_status()
     room.start()
