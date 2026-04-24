85d50f087054a7f986ad34a5fd59a8b7e776fa55
MufeeSama <wntee@qq.com>
2026-04-25 04:07:53 +0800

    chore: update main.py and requirements.txt

--- main.py
+++ main.py
@@ -9,7 +9,7 @@
 from liveMan import DouyinLiveWebFetcher
 
 if __name__ == '__main__':
-    live_id = '510200350291'
+    live_id = '870592768542'
     room = DouyinLiveWebFetcher(live_id)
     # room.get_room_status() # 失效
     room.start()
