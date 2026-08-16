7da59ed96961ec76651e374e125a576f19628385
cangyin <excangyin@gmail.com>
2026-08-16 23:47:42 +0800

    解析更多消息类型

--- liveMan.py
+++ liveMan.py
@@ -135,7 +135,8 @@ def generateMsToken(length=182):
     return random_str
 
 
-def timestamp_to_readable_str(ts: int, mili=False):
+def format_readable_time(ts: int, mili=False):
+    ts = int(ts)
     if mili:
         return datetime.fromtimestamp(ts / 1000).strftime('%Y-%m-%d %H:%M:%S.' + str(ts % 1000))
     else:
@@ -195,7 +196,7 @@ class DouyinLiveWebFetcher:
     def start(self, display_text: str, data: str):
         self._init_log_files()
         self.stopped = False
-        self.log_json(timestamp_to_readable_str(int(time.time() * 1000), True))
+        self.log_json(format_readable_time(int(time.time() * 1000), True))
         self.log_json(data)
         self.log_msg(f'【直播间】{display_text}')
         self._connectWebSocket()
@@ -472,12 +473,16 @@ class DouyinLiveWebFetcher:
             'WebcastGiftSortMessage': self._muteMsg,  # UI礼物布局，无可用信息
             'WebcastChatLikeMessage': self._muteMsg,  # 未知
             'WebcastLuckyBoxTempStatusMessage': self._muteMsg,  # 无可用信息
-            'WebcastBattleStatusMessage': self._muteMsg,  # 无对应 Message 定义
+            'WebcastBattleStatusMessage': self._parseBattleStatusMessage,
             'WebcastLinkerContributeMessage': self._muteMsg,  # 未知
-            'WebcastProfitInteractionScoreMessage': self._muteMsg,  # 利润互动分数？
+            'WebcastProfitInteractionScoreMessage': self._muteMsg,  # 收益性互动分数？
             'WebcastRanklistHourEntranceMessage': self._muteMsg,  # 小时榜、百强榜。人气榜呢？
             'WebcastBattleTeamTaskMessage': self._muteMsg,  # PK分数加成消息
+            'WebcastEasterEggDataMessage': self._muteMsg,  # 未知
+            'WebcastDecorationModifyMethod': self._muteMsg,  # 未知
+            'WebcastDecorationUpdateMessage': self._muteMsg,  # 未知
         }
+        muted_messages = [k for k, v in method_bindings.items() if v == self._muteMsg]
 
         # 根据proto结构体解析对象
         try:
@@ -504,7 +509,10 @@ class DouyinLiveWebFetcher:
             ws.send(ack, websocket.ABNF.OPCODE_BINARY)
 
         if len(response.messages) > 0:
-            self.cascade_log(timestamp_to_readable_str(response.now, True))
+            self.log_json(format_readable_time(response.now, True))
+            msgs = [m for m in response.messages if m.method not in muted_messages]
+            if len(msgs) > 0:
+                self.log_msg(format_readable_time(response.now, True))
 
         # 根据消息类别解析消息体
         for msg in response.messages:
@@ -683,7 +691,7 @@ class DouyinLiveWebFetcher:
         num = f'{cjrp.left_num}/{cjrp.total_num}'
         amount = f'{cjrp.left_amount}/{cjrp.total_amount}'
         currency = '钻' if cjrp.currency == 'Diamond' else cjrp.currency
-        timing = f'时间 {timestamp_to_readable_str(cjrp.send_time)} ~ {timestamp_to_readable_str(cjrp.expire_time)}'
+        timing = f'时间 {format_readable_time(cjrp.send_time)} ~ {format_readable_time(cjrp.expire_time)}'
         self.log_msg(f"【红包】{cjrp.text} 剩余 {num}个，价值 {amount}({currency})，{timing}")
 
     def _parseLuckyBoxRewardMessage(self, payload):
@@ -705,7 +713,7 @@ class DouyinLiveWebFetcher:
         m = self._parseFromString('LinkMessage', payload)
         oneof = m.WhichOneof('content')
         if not oneof:
-            self.log_msg(f"Unrecognized LinkMessage: content oneof={oneof}, dump: {self._MessageToJson(m)}")
+            self.log_msg(f"【连线】  Unrecognized LinkMessage: content oneof={oneof}, dump: {self._MessageToJson(m)}")
             return
 
         def print_linked_users(fmt: str, linked_users: List) -> Tuple[int, str]:
@@ -753,33 +761,30 @@ class DouyinLiveWebFetcher:
         m = self._parseFromString('CommonToastMessage', payload)
         self.log_msg(f"【弹出提示】{render_text(m.common.display_text)}")
 
-    def _parseRoomDataSyncMessage(self, payload):
-        m = self._parseFromString('RoomDataSyncMessage', payload)
+    def _parseRoomDataSyncMessage(self, payload_):
+        m = self._parseFromString('RoomDataSyncMessage', payload_)
+        payload = self._parseFromString(m.syncKey, m.payload)
+        self.log_json(f'  {m.syncKey}={self._MessageToJson(payload)}')
         handled = False
         if m.syncKey == 'RoomLinkMicSyncData':
-            linkmic = self._parseFromString(m.syncKey, m.payload)
             link_type_map = {1: '视频连线', 2: '语音连线'}
             users = []
-            for e in linkmic.linked_users:
+            for e in payload.linked_users:
                 users.append(f'{e.user.nickname}({e.user.id})')
                 if e.link_type in link_type_map:
                     users[-1] += ' ' + link_type_map[e.link_type]
             self.log_msg(f"【连线状态同步】{len(users)}位连线用户：{' │ ' .join(users)}")
-            self.cascade_log(f'  {m.syncKey}={self._MessageToJson(linkmic)}')
             handled = True
         elif m.syncKey == 'LotteryInfoSyncData':
-            lottery = self._parseFromString(m.syncKey, m.payload)
-            timing = f'时间 {timestamp_to_readable_str(lottery.start_time)} ~ {timestamp_to_readable_str(lottery.draw_time)}'
+            lottery = payload
+            timing = f'时间 {format_readable_time(lottery.start_time)} ~ {format_readable_time(lottery.draw_time)}'
             if lottery.lottery_type == 1:
                 self.log_msg(
                     f"【福袋】{lottery.lucky_count}个，{lottery.prize_count}钻，{lottery.candidate_total_count}人参与，{timing}")
-                self.cascade_log(f'  {m.syncKey}={self._MessageToJson(lottery)}')
                 handled = True
         elif m.syncKey == 'HighlightContainerSyncData':
-            highlight_container = self._parseFromString(m.syncKey, m.payload)
-            self.log_json(f'  {m.syncKey}={self._MessageToJson(highlight_container)}')
             all_parsed = True
-            for item in highlight_container.highlight_items:
+            for item in payload.highlight_items:
                 if item.data_type == 4:
                     if item.end_time > time.time():
                         continue
@@ -788,14 +793,15 @@ class DouyinLiveWebFetcher:
                     all_parsed = False
             handled = all_parsed
         elif m.syncKey == 'DoubleLikeSyncData':
-            data = self._parseFromString(m.syncKey, m.payload)
-            self.log_json(f'  {m.syncKey}={self._MessageToJson(data)}')
-            self.log_msg(f"【双倍点赞】{render_text(data.normal_display_text)}")
+            self.log_msg(f"【双倍点赞】{render_text(payload.normal_display_text)}")
             handled = True
+        elif m.syncKey == 'PreviewPromotionSyncData':
+            if payload.type == 2 and payload.lucky_money.display_end_at < time.time():
+                self.log_msg(f"【红包信息】{payload.lucky_money.text}")
+                handled = True
 
         if not handled:
-            self.log_msg(f'【RoomDataSyncMessage】', end='')
-            self.cascade_log(f'  {m.syncKey}={self._tryDumpJson(m.syncKey, m.payload)}')
+            self.log_msg(f'【RoomDataSyncMessage】  {m.syncKey}={self._tryDumpJson(m.syncKey, m.payload)}')
 
     def _parseRoomCommentTopicMessage(self, payload):
         m = self._parseFromString('RoomCommentTopicMessage', payload)
@@ -810,8 +816,8 @@ class DouyinLiveWebFetcher:
 
     def _parseAnchorLinkmicSilenceMessage(self, payload):
         m = self._parseFromString('AnchorLinkmicSilenceMessage', payload)
-        silence_action_map = {1: '静音', 2: '取消静音'}
-        self.log_msg(f'【静音】{m.from_user_id} 将 {m.to_user_id} {silence_action_map[m.silence_action]} 了')
+        silence_action = {1: '静音', 2: '取消静音'}.get(m.silence_action)
+        self.log_msg(f'【静音】{m.from_user_id} 将 {m.to_user_id} {silence_action} 了')
 
     def _parseNotifyMessage(self, payload):
         m = self._parseFromString('NotifyMessage', payload)
@@ -848,7 +854,16 @@ class DouyinLiveWebFetcher:
             handled = True
 
         if not handled:
-            if ('25v_11_ai_gift' in extra) or ('26w_5_tier' in extra) or (
-                    '23v_8_anchor_flow' in extra) or ('26v_6_game' in extra):
+            if ('多阶段任务_' in m.extra) or ('万粉团' in m.extra) or (
+                '新星I' in m.extra
+            ):
                 return
             self.log_msg(f'【InRoomBannerMessage】  {self._MessageToJson(m)}')
+
+    def _parseBattleStatusMessage(self, payload):
+        m = self._parseFromString('BattleStatusMessage', payload)
+        status = {1: '开始', 2: '惩罚', 3: '结束'}.get(m.status, f'状态 {m.status}')
+        duration = (int(m.end_time_ms) - int(m.start_time_ms)) // 1000
+        punish_hint = f'，惩罚时长{m.punish_duration}秒' if m.status == 2 else ''
+        self.log_msg(
+            f'【PK {status}】时间{duration}秒，{format_readable_time(m.start_time_ms[:-3])} ~ {format_readable_time(m.end_time_ms[:-3])}{punish_hint}')
