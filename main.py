22e02229263a5cb5fad02b87ee64256115433590
saermart <kukushka@126.com>
2026-08-26 16:13:55 +0800

    编译dylive-v1.0.0

--- main.py
+++ main.py
@@ -9,7 +9,7 @@
 from liveMan import DouyinLiveWebFetcher
 
 if __name__ == '__main__':
-    live_id = '510200350291'
+    live_id = '203455341794'
     room = DouyinLiveWebFetcher(live_id)
     # room.get_room_status() # 失效
     room.start()
